72dd9a8ce2a235e80d9f50ef2ffe83d11bd0ba3e
Erwin Bonnet <erwinbonnet@gmail.com>
2013-09-04 13:41:13 +0200

    Start making a bit of a layout

---
+++ src/Web.UI/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Web.UI.Models;
+
+namespace Web.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model, string returnUrl)
+        {
+            return View();
+        }
+    }
+}
