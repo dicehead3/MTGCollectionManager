15c656a542319e2acb2c2550a70010484c93c6d6
Erwin Bonnet <erwinbonnet@gmail.com>
2013-09-09 15:17:44 +0200

    Reference fix

--- Test/GathererRipper/WindowsFormsApplication1/Form1.cs
+++ Test/GathererRipper/WindowsFormsApplication1/Form1.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
@@ -16,81 +13,89 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
 
-            const string url = "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid={0}";
-            var cards = new List<CardData>();
-            var threads = new List<Thread>();
-            var startTime = DateTime.Now.Millisecond;
-            for (var i = 0; i < 2000; i += 4)
-            {
-                var t1 = new Thread(() =>
-                {
-                    var result = GetCardDetails(string.Format(url, i));
-                    if (!string.IsNullOrEmpty(result))
-                        cards.Add(new CardData { Id = i, Title = result });
-                    Thread.Sleep(50);
-                });
+            //const string url = "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid={0}";
+            //var cards = new List<CardData>();
+            //var threads = new List<Thread>();
+            //var startTime = DateTime.Now;
+            //for (var i = 0; i < 5000; i += 6)
+            //{
+            //    var t1 = new Thread(() =>
+            //    {
+            //        var result = GetCardDetails(string.Format(url, i));
+            //        if (!string.IsNullOrEmpty(result))
+            //            cards.Add(new CardData { Id = i, Title = result });
+            //        Thread.Sleep(50);
+            //    });
 
-                var t2 = new Thread(() =>
-                {
-                    var result = GetCardDetails(string.Format(url, i + 1));
-                    if (!string.IsNullOrEmpty(result))
-                        cards.Add(new CardData { Id = i + 1, Title = result });
-                    Thread.Sleep(50);
-                });
+            //    var t2 = new Thread(() =>
+            //    {
+            //        var result = GetCardDetails(string.Format(url, i + 1));
+            //        if (!string.IsNullOrEmpty(result))
+            //            cards.Add(new CardData { Id = i + 1, Title = result });
+            //    });
 
-                var t3 = new Thread(() =>
-                {
-                    var result = GetCardDetails(string.Format(url, i + 2));
-                    if (!string.IsNullOrEmpty(result))
-                        cards.Add(new CardData { Id = i + 2, Title = result });
-                    Thread.Sleep(50);
-                });
+            //    var t3 = new Thread(() =>
+            //    {
+            //        var result = GetCardDetails(string.Format(url, i + 2));
+            //        if (!string.IsNullOrEmpty(result))
+            //            cards.Add(new CardData { Id = i + 2, Title = result });
+            //    });
 
-                var t4 = new Thread(() =>
-                {
-                    var result = GetCardDetails(string.Format(url, i + 3));
-                    if (!string.IsNullOrEmpty(result))
-                        cards.Add(new CardData { Id = i + 3, Title = result });
-                    Thread.Sleep(50);
-                });
+            //    var t4 = new Thread(() =>
+            //    {
+            //        var result = GetCardDetails(string.Format(url, i + 3));
+            //        if (!string.IsNullOrEmpty(result))
+            //            cards.Add(new CardData { Id = i + 3, Title = result });
+            //    });
 
-                var t5 = new Thread(() =>
-                {
-                    var result = GetCardDetails(string.Format(url, i + 3));
-                    if (!string.IsNullOrEmpty(result))
-                        cards.Add(new CardData { Id = i + 3, Title = result });
-                    Thread.Sleep(50);
-                });
+            //    var t5 = new Thread(() =>
+            //    {
+            //        var result = GetCardDetails(string.Format(url, i + 4));
+            //        if (!string.IsNullOrEmpty(result))
+            //            cards.Add(new CardData { Id = i + 4, Title = result });
+            //    });
 
-                var t6 = new Thread(() =>
-                {
-                    var result = GetCardDetails(string.Format(url, i + 3));
-                    if (!string.IsNullOrEmpty(result))
-                        cards.Add(new CardData { Id = i + 3, Title = result });
-                    Thread.Sleep(50);
-                });
+            //    var t6 = new Thread(() =>
+            //    {
+            //        var result = GetCardDetails(string.Format(url, i + 5));
+            //        if (!string.IsNullOrEmpty(result))
+            //            cards.Add(new CardData { Id = i + 5, Title = result });
+            //    });
 
-                t1.Start();
-                t2.Start();
-                t3.Start();
-                t4.Start();
-                t5.Start();
-                t6.Start();
+            //    t1.Start();
+            //    t2.Start();
+            //    t3.Start();
+            //    t4.Start();
+            //    t5.Start();
+            //    t6.Start();
 
-                t1.Join();
-                t2.Join();
-                t3.Join();
-                t4.Join();
-                t5.Join();
-                t6.Join();
-            }
+            //    t1.Join();
+            //    t2.Join();
+            //    t3.Join();
+            //    t4.Join();
+            //    t5.Join();
+            //    t6.Join();
+            //}
 
-            foreach (var c in cards.OrderBy(x => x.Id).ToList())
+            //foreach (var c in cards.OrderBy(x => x.Id).ToList())
+            //{
+            //    listBox1.Items.Add(string.Format("{0} -- {1}", c.Id, c.Title));
+            //}
+
+            //var stopTime = DateTime.Now;
+            //var difference = stopTime - startTime;
+            //listBox1.Items.Add("");
+            //listBox1.Items.Add(string.Format("Time: {0}:{1}:{2}.{3}", difference.Hours, difference.Minutes, difference.Seconds, difference.Milliseconds));
+
+            var list = ListCreator.CreateCardList();
+            var diff = ListCreator.endTime - ListCreator.startTime;
+
+            foreach (var c in list.OrderBy(x=>x.Id).ToList())
             {
                 listBox1.Items.Add(string.Format("{0} -- {1}", c.Id, c.Title));
             }
             listBox1.Items.Add("");
-            listBox1.Items.Add(string.Format("Time: {0}", DateTime.Now.Millisecond - startTime));
+            listBox1.Items.Add(string.Format("Time: {0}:{1}:{2}.{3}", diff.Hours, diff.Minutes, diff.Seconds, diff.Milliseconds));
         }
 
         private static string GetCardDetails(string url)
---
+++ Test/GathererRipper/WindowsFormsApplication1/ListCreator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class ListCreator
+    {
+        public static DateTime startTime;
+        public static DateTime endTime;
+
+        private static List<CardData> cards = new List<CardData>();
+        public static List<CardData> CreateCardList()
+        {
+            int cardCount = 1636;
+            Task[] tasks = new Task[cardCount];
+            int counter = 0;
+            startTime = DateTime.Now;
+            for (; counter < cardCount; counter++)
+            {
+                int c = counter;
+                var task = Task.Factory.StartNew(() => GetTitle(c), TaskCreationOptions.LongRunning);
+                tasks[c] = task;
+            }
+
+            Task.WaitAll(tasks);
+            endTime = DateTime.Now;
+            return cards;
+        }
+
+
+        private static void GetTitle(int id)
+        {
+            var req = (HttpWebRequest) WebRequest
+                .Create(string.Format("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid={0}", id));
+            var resp = (HttpWebResponse) req.GetResponse();
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                var stream = resp.GetResponseStream();
+                var res = resp.CharacterSet == null
+                    ? new StreamReader(stream).ReadToEnd()
+                    : new StreamReader(stream, Encoding.GetEncoding(resp.CharacterSet)).ReadToEnd();
+                resp.Close();
+                stream.Close();
+
+                var match = Regex.Match(res, @"<title>\s*(.+?)\s*</title>");
+                if (match.Success)
+                {
+                    var title = match.Groups[1].Value;
+                    title = title.Remove(title.IndexOf("Gatherer"));
+                    if (!string.IsNullOrEmpty(title))
+                    {
+                        var cd = new CardData
+                        {
+                            Id = id,
+                            Title = title.Remove(title.Length - 3)
+                        };
+                        cards.Add(cd);
+                    }
+                }
+            }
+        }
+    }
+}
